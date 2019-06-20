e1d934cacd572d6123e735a4a3e508d3d29cde42
yanjiantao <boolean.yan@gmail.com>
2019-06-20 18:19:10 +0800

    :sparkles: spring-security-simple

---
+++ spring-security-simple/src/main/java/com/tao/java/example/springsecuritysimple/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.tao.java.example.springsecuritysimple.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Descriiption TODO
+ * @Author yanjiantao
+ * @Date 2019/6/20 15:18
+ **/
+@Slf4j
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/")
+    public String index() {
+        log.info("----------->index");
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        log.info("----------->hello");
+        return "hello";
+    }
+}
---
+++ spring-security-simple/src/test/java/com/tao/java/example/springsecuritysimple/SpringSecuritySimpleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tao.java.example.springsecuritysimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecuritySimpleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
